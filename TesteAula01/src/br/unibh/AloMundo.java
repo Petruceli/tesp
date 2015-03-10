45ad665bfd9bcaa4f8697aefb321e77051c0dd17
Gustavo <Gustavo@10.13.10.64>
2015-03-10 20:45:02 -0300

    Conexão Banco de dados funcionando.

--- TesteAula01/src/br/unibh/AloMundo.java
+++ TesteAula01/src/br/unibh/AloMundo.java
@@ -1,10 +1,13 @@
 package br.unibh;
 
 import java.math.BigDecimal;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Date;
 
 import br.unibh.entidades.Aluno;
 import br.unibh.entidades.Professor;
+import br.unibh.persistencia.JDBCUtil;
 
 public class AloMundo {
 
@@ -19,19 +22,31 @@ public class AloMundo {
 		Aluno a1 = new Aluno(1L, 1234L, "João", "987987987", new Date());
 		Aluno a2 = new Aluno(2L, 5678L, "Maria", "345345345", new Date());
 		Aluno a3 = new Aluno(3L, 8566L, "Renata", "767676767");
-		Aluno a4 = new Aluno(4L, 9900L, "Gustavo");
-		Professor p1 = new Professor(5L, "José", "123123123",
-				new BigDecimal(123456));
+		Professor p1 = new Professor(5L, "José", "123123123", new BigDecimal(
+				123456));
 		Professor p2 = new Professor(6L, "Luiz", "765765765");
-		
+
 		System.out.println("Bonus ====== " + Professor.BONUS);
-		System.out.println("Matrícula é " + Aluno.verificaMatricula("1111111111111"));
-		
+		System.out.println("Matrícula é "
+				+ Aluno.verificaMatricula("1111111111111"));
+
 		System.out.println(a1);
 		System.out.println(a2);
 		System.out.println(a3);
 		System.out.println(p1);
 		System.out.println(p2);
+
+		try {
+			ResultSet res = JDBCUtil.getConnection()
+					.prepareStatement("select * from tb_aluno").executeQuery();
+			while (res.next()){
+				System.out.println(res.getLong("id")+"\t"+res.getString("nome"));
+			}		
+			JDBCUtil.closeConnection();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 }
